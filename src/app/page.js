2c01d25dd9427f74ea9509a7807ef5fbb755e939
replyre <121796450+replyre@users.noreply.github.com>
2024-07-17 23:36:25 +0530

    header component and dark mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import Header from "@/components/Header";
 
 export default function Component() {
-  return <ThemeProvider attribute="class">Nexcent app</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      <Header />
+    </ThemeProvider>
+  );
 }
