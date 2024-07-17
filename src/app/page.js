3dd7ec8c1ecfe5b6280cf1d95e90f27b3c7514eb
replyre <121796450+replyre@users.noreply.github.com>
2024-07-18 03:10:09 +0530

    counter component with countup animation

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,16 @@
 import { ThemeProvider } from "next-themes";
 import Header from "@/components/Header";
 import Carousal from "@/components/carousal";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Counter from "@/components/Counter";
 export default function Component() {
   return (
     <ThemeProvider attribute="class">
       <Header />
       <Carousal />
+
+      <Counter />
     </ThemeProvider>
   );
 }
