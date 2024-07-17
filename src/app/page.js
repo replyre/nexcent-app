cbd6d790006118bc10ab558aee88340af095edef
replyre <121796450+replyre@users.noreply.github.com>
2024-07-18 00:39:33 +0530

    carousal and images

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 
 import { ThemeProvider } from "next-themes";
 import Header from "@/components/Header";
+import Carousal from "@/components/carousal";
 
 export default function Component() {
   return (
     <ThemeProvider attribute="class">
       <Header />
+      <Carousal />
     </ThemeProvider>
   );
 }
